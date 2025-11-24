f7261727bfd28169277a074d22c9c7eb597f2ff2
Madhusudan Gurjar <161649945+Madhusudan-Gurjar@users.noreply.github.com>
2025-11-24 19:12:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n=arr.length;
+        int i,res=0;
+        if(n==3 && arr[1]>arr[0] && arr[2]< arr[1]){
+            return 1;
+        }else if(n==2 && arr[1]>arr[0]){
+            return 1;
+        }else if(n==2 && arr[1]<arr[0]){
+            return 0;
+        }else{
+            for(i=0;i<n-1;i++){
+                if(arr[i+1]<arr[i]){
+                 res = i;
+                }
+            }
+        }
+        // for(i=0;i<n-1;i++){
+        //     if(arr[i+1]<arr[i]){
+        //          res = i-1;
+        //     }
+        // }
+        
+    return res;
+    }
+}
